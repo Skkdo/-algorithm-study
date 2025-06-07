ecc6a9458406021ea9c010715bdb426922661bb1
JinHyoung <104055567+Skkdo@users.noreply.github.com>
2025-06-07 13:01:08 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,30 @@
+WITH RECURSIVE GENERATION_TREE AS (
+    SELECT
+        ID,
+        PARENT_ID,
+        1 AS GENERATION
+    FROM
+        ECOLI_DATA
+    WHERE
+        PARENT_ID IS NULL
+    
+    UNION ALL
+    
+    SELECT
+        n.ID,
+        n.PARENT_ID,
+        t.GENERATION + 1 AS GENERATION
+    FROM
+        ECOLI_DATA n
+    JOIN
+        GENERATION_TREE t ON n.PARENT_ID = t.ID
+)
+
+SELECT 
+    ID
+FROM 
+    GENERATION_TREE
+WHERE 
+    GENERATION = 3
+ORDER BY ID
+
