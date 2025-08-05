e639ea911fc5b814951350aabc7d9bec9813952e
JinHyoung <104055567+Skkdo@users.noreply.github.com>
2025-08-05 13:07:33 +0900

    [level 2] Title: 소수 찾기, Time: 25.29 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,39 @@
 import java.util.*;
+
 class Solution {
-    
-    static boolean flag = true;     
+
+    Set<Integer> set = new HashSet<>();
     
     public int solution(String numbers) {
-        int answer = 0;
-        int[] check = new int[10];
-        Integer[] numList = new Integer[numbers.length()];
-
-        for (int i = 0; i < numbers.length(); i++) {
-            int num = numbers.charAt(i) - '0';
-            numList[i] = num;
-            check[num]++;
-        }
-        Arrays.sort(numList, (o1, o2) -> o2 - o1);
-        int max = 0;
-        for (int i = 0; i < numList.length; i++) {
-            max = max * 10 + numList[i];
-        }
-
-        boolean[] decimal = new boolean[max + 1];
-        for (int i = 2; i <= Math.sqrt(max); i++) {
-            if (decimal[i]) continue;
-            for (int j = 2; j <= max / i; j++) {
-                decimal[i * j] = true;
-            }
-        }
-
-        for (int i = 2; i <= max; i++) {
-            if (decimal[i]) continue;
-
-            flag = true;
-            int[] arr = check.clone();
-            test(i, arr);
-            if (flag) answer++;
-        }
+        boolean[] check = new boolean[numbers.length()];
+        DFS(numbers,"",check);
         
+        int answer = 0;
+        for(Integer num : set) {
+            if(isPrime(num)) answer++;
+        }
         return answer;
     }
-    public static void test(int num, int[] check) {
-        if (!flag) return;
-        if (num >= 10) test(num / 10, check);
-        if (num < 10) {
-            if (0 < check[num]--)  return;
+    
+    public void DFS(String numbers, String string, boolean[] check) {
+        if(!string.equals("")){
+            set.add(Integer.parseInt(string));
         }
-        if (0 < check[num % 10]--)  return;
-        flag = false;
+        
+        for(int i = 0; i < numbers.length(); i++) {
+            if(!check[i]) {
+                check[i] = true;
+                DFS(numbers, string + numbers.charAt(i), check);
+                check[i] = false;
+            }
+        }
+    }
+    
+    public boolean isPrime(Integer num) {
+        if(num < 2) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 }
