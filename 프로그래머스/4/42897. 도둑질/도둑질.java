ced25292cbc9f6ae7d79100b93c394f65f8649be
JinHyoung <104055567+Skkdo@users.noreply.github.com>
2025-08-13 18:34:55 +0900

    [level 4] Title: 도둑질, Time: 27.86 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] money) {
+        int max = 0;
+        
+        int[] dp = new int[money.length];
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0], money[1]);
+        for(int i = 2; i < money.length - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+        max = dp[money.length - 2];
+        
+        dp = new int[money.length];
+        dp[0] = 0;
+        dp[1] = money[1];
+        for(int i = 2; i < money.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+        }
+        
+        max = Math.max(max, dp[money.length - 1]);
+        return max;
+    }
+}
