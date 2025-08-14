031172be0f44b27f5e8bd817796c15a7d74cdb24
JinHyoung <104055567+Skkdo@users.noreply.github.com>
2025-08-14 12:20:44 +0900

    [level 2] Title: 타겟 넘버, Time: 182.86 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,21 @@
+import java.util.*;
+
 class Solution {
-    static int num;
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        int[][] map = new int[numbers.length][2];
-        for(int i=0;i<numbers.length;i++){
-            map[i][0] = numbers[i];
-            map[i][1] = numbers[i]*-1;
-        }
-        num =0;
-        DFS(0,target,0,map);
-        answer = num;
-        return answer;
-    }
-    public static void DFS(int index , int target, int tmp ,int[][] map){
-        if(index == map.length){
-            if(tmp == target) num++;
-            return;
-        }
-        for(int i=0;i<map[index].length;i++){
-            tmp += map[index][i];
-            DFS(index+1,target,tmp,map);
-            tmp -= map[index][i];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        
+        for(int i = 0; i < numbers.length; i++) {
+            Integer num = numbers[i];
+            for(int size = q.size(); size > 0; size--) {
+                Integer poll = q.poll();
+                q.add(poll + num);
+                q.add(poll - num);
+            }  
         }
         
+        while(!q.isEmpty()) if(q.poll() == target) answer++;
+        return answer;
     }
 }
